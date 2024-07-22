dfb64fa0fd33c7fa4e5d31b89ad2a601d7dc7487
Григорий <122614065+Grigorasch@users.noreply.github.com>
2024-07-22 06:55:12 +0000

    feat: Настроен https протокол

--- src/server.js
+++ src/server.js
@@ -1,8 +1,30 @@
+require('dotenv').config();
+const fs = require('fs');
+const http = require('http');
+const https = require('https');
 const app = require("./app");
 
-const port = 80;
-const host = "46.8.236.43";
+const port = process.env.SERVER_PORT;
+const host = process.env.SERVER_HOST;
 
-app.listen(port, host, () => {
-  console.log(`Example app listening on host ${host} and port ${port}`);
+// Пути к сертификатам
+const privateKey = fs.readFileSync('/etc/letsencrypt/live/grigorash.site/privkey.pem', 'utf8');
+const certificate = fs.readFileSync('/etc/letsencrypt/live/grigorash.site/cert.pem', 'utf8');
+const ca = fs.readFileSync('/etc/letsencrypt/live/grigorash.site/chain.pem', 'utf8');
+const credentials = { key: privateKey, cert: certificate, ca: ca };
+
+// HTTPS сервер
+const httpsServer = https.createServer(credentials, app);
+httpsServer.listen(port, host, () => {
+  console.log(`HTTPS server listening on host ${host} and port ${port}`);
 });
+// HTTP сервер для редиректов на HTTPS
+const httpServer = http.createServer((req, res) => {
+  res.writeHead(301, { "Location": `https://${req.headers.host}${req.url}` });
+  res.end();
+});
+httpServer.listen(80, host, () => {
+  console.log(`HTTP server listening on host ${host} and port 80 for redirection`);
+});
+
+
