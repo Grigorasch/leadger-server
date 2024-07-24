777dcf16df880a314f669282602fe271c4b5babc
Григорий <122614065+Grigorasch@users.noreply.github.com>
2024-07-24 12:48:49 +0000

    feat

--- src/routes/authentication.js
+++ src/routes/authentication.js
@@ -1,7 +1,7 @@
 const authRouter = require('express').Router();
 
-const bcrypt = require('bcrypt');
 const User = require('../models/user');
+const authController = require('../controllers/authController');
 
 authRouter.get('/g' , async (req, res) => {
 const user = await User.build({
@@ -15,29 +15,11 @@ const user = await User.build({
   } catch (error) {
      console.log(error);
   }
-  
   console.log(user)
   
   res.send('g');
 });
 
-authRouter.post('/signup', async (req, res) => {
-  const {email, password, password_confirmation, name } = req.body;
-
-  
-  try {
-    const user = await User.findOne({ username });
-    if (user) {
-      return res.status(400).send('Username already exists');
-    }
-    const hashedPassword = await bcrypt.hash(password, 10);
-    const newUser = new User({ username, password: hashedPassword });
-    await newUser.save();
-    res.send('User registered successfully');
-  } catch (error) {
-    console.error(error);
-    res.status(500).send('Internal server error');
-  }
-});
+authRouter.post('/signup', authController.signup);
 
 module.exports = authRouter;
