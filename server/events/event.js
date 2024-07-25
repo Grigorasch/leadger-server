5005208daeb0394a6195fd0c1f3af657e527a1bb
Григорий <122614065+Grigorasch@users.noreply.github.com>
2024-07-25 21:06:53 +0000

    docs: Добавлена документация

--- server/events/event.js
+++ server/events/event.js
@@ -1,4 +1,15 @@
+/**
+ * Класс, представляющий событие.
+ */
 class Event {
+  /**
+   * Создает экземпляр Event.
+   * @param {string} name - Имя события.
+   * @param {Object} [options={}] - Дополнительные параметры.
+   * @param {Object} [options.target] - Объект цели события.
+   * @param {Event} [options.previousEvent] - Предыдущее событие.
+   * @param {EventEmitter} [options.EventEmitter] - Экземпляр EventEmitter для генерации события.
+   */
   constructor(name, { target, previousEvent, EventEmitter } = {}) {
     this.name = name;
     this.date = new Date();
