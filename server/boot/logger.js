1c470b10bbe51b51508671fc6824d14a17527638
Григорий <122614065+Grigorasch@users.noreply.github.com>
2024-07-26 01:27:07 +0000

    refact: Объединен код

--- server/boot/logger.js
+++ server/boot/logger.js
@@ -1,9 +1,55 @@
+const winston = require('winston');
+
+/**
+ * Объект логгера с методами для различных уровней логирования. По умолчанию,
+ * логи раставлены везде где нужно, но никакую информацию не передают т.к.
+ * представляют из себя пустую стрелочную функцию.
+ * В ситуациях, когда нужно передавать какую-то информацию, функция логгера
+ * заменяется на полноценный логгер.
+ * @namespace logger
+ */
 const logger = {
-  info: () => {},
-  warning: () => {},
-  error: () => {},
-  debug: () => {}
-}
+    /**
+     * Логгирование информационного сообщения
+     * @function
+     */
+    info: () => {},
+    /**
+     * Логгирование предупреждающего сообщения
+     * @function
+     */
+    warning: () => {},
+    /**
+     * Логгирование сообщения об ошибке
+     * @function
+     */
+    error: () => {},
+    /**
+     * Логгирование отладочного сообщения
+     * @function
+     */
+    debug: () => {}
+};
 
+/**
+ * Создание логгера с указанным уровнем
+ * @function
+ * @param {string} level - Используемый уровень логирования
+ * @returns {winston.Logger} Настроенный экземпляр логгера
+ */
+const createLogger = (level) => {
+    return winston.createLogger({
+        level: level,
+        format: winston.format.combine(
+            winston.format.timestamp(),
+            winston.format.printf(({ timestamp, level, message }) => {
+                return `${timestamp} [${level.toUpperCase()}]: ${message}`;
+            })
+        ),
+        transports: [
+            new winston.transports.Console()
+        ]
+    });
+};
 
-module.exports = logger;
+module.exports = {logger, createLogger};
