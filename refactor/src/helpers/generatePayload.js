c3e0ac20b3912408aa3e0cde59a77db31407f412
Григорий <122614065+Grigorasch@users.noreply.github.com>
2024-08-04 21:11:21 +0000

    chore: Файлы удалены

--- refactor/src/helpers/generatePayload.js
+++
@@ -1,8 +0,0 @@
-module.exports = function generatePayload(req) {
-  return {
-    iss: "grsch",
-    sub: req.socket.id,
-    exp: Date.now() + 60 * 60 * 1000,
-    iat: Date.now(),
-  };
-};
--- refactor/src/routes/authentication.js
+++
@@ -1,9 +0,0 @@
-const authRouter = require('express').Router();
-const authController = require('../controllers/authController');
-
-authRouter.post('/signup', authController.signup);
-authRouter.post('/l', (req, res) => {
-  res.status(200).json(req)
-});
-
-module.exports = authRouter;
