caf81caab9844c49fde156cadd28081555a8ee1b
Григорий <122614065+Grigorasch@users.noreply.github.com>
2024-08-06 12:52:18 +0000

    feat: Изменена архитектура файла main
    В первую очередь производится чтение атрибутов запуска и чтение конфига
    Затем производится настройка сервера в соответствии с конфигом
    Затем создаётся сущность сервера

--- server/main.js
+++ server/main.js
@@ -1,22 +1,47 @@
 const argv = require("yargs").argv;
-const configer = require("./boot/configer");
-const { logger, createLogger } = require("./boot/logger");
-const serverManager = require("./control/server-manager");
+const { c, m } = argv;
+const { loadConfig } = require("./boot/config");
+let serverManager;
 
-async function main() {
-  // Получаем параметры запуска
-  const { config, manual } = argv;
-  // Загружаем конфигурацию
-  global.config = await configer.loadConfig(config, manual);
+loadConfig(c, m).then((config) => {
+  global.config = config;
+  preloading();
+});
+
+function preloading() {
   // Создаем логгер
   if (global.config.log.enabled) {
+    const { logger, createLogger } = require("./boot/logger");
     global.config.log.level.map((level) => {
       preparedLogger = createLogger(level);
       logger[level] = (message) => preparedLogger[level](message);
     });
     logger.info("Логирование включено");
   }
-  serverManager.status = serverManager.SERVER_STATES[(global.config.autostart ? 2 : 3)];
+
+  // Запуск сервера если Автозапуск включен, иначе переход в режим ожидания
+  serverManager = require("./control/server-manager");
+  serverManager.state =
+    serverManager.SERVER_STATES[global.config.autostart ? 2 : 3];
+  console.log(serverManager.state);
 }
 
-main()
+// main()
+
+// const { logger, createLogger } = require("./boot/logger");
+// const serverManager = require("./control/server-manager");
+
+// async function main() {
+//   // Получаем параметры запуска
+//   // Загружаем конфигурацию
+//   global.config = await configer.loadConfig(config, manual);
+//   // Создаем логгер
+//   if (global.config.log.enabled) {
+//     global.config.log.level.map((level) => {
+//       preparedLogger = createLogger(level);
+//       logger[level] = (message) => preparedLogger[level](message);
+//     });
+//     logger.info("Логирование включено");
+//   }
+//   serverManager.status = serverManager.SERVER_STATES[(global.config.autostart ? 2 : 3)];
+// }
